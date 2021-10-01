7c47cb35e0d7e41ce65272fdb611e5caa99ef8bb
Nikola Okuka <nikolaokuka@gmail.com>
2021-10-01 07:38:02 +0200

    add PlayerInput component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
+import PropTypes from 'prop-types'
 
 function Instructions() {
   return (
@@ -25,11 +26,92 @@ function Instructions() {
   )
 }
 
-export default class Battle extends Component {
+class PlayerInput extends Component {
+  state = {
+    username: ''
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.onSubmit(this.state.username)
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
   render() {
+    return (
+      <form className='column player' onSubmit={this.handleSubmit}>
+        <label htmlFor='username' className='player-label'>
+          {this.props.label}
+        </label>
+        <div className='row player-inputs'>
+          <input
+            type='text'
+            id='username'
+            className='input-light'
+            placeholder='github username'
+            autoComplete='off'
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button
+            className='btn btn-dark'
+            type='submit'
+            disabled={!this.state.username}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    )
+  }
+}
+
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+}
+
+export default class Battle extends Component {
+  state = {
+    playerOne: null,
+    playerTwo: null
+  }
+
+  handleSubmit = (id, username) => {
+    this.setState({
+      [id]: username
+    })
+  }
+
+  render() {
+    const { playerOne, playerTwo } = this.state
+
     return (
       <>
         <Instructions />
+
+        <div className='players-container'>
+          <h1 className='center-text header-lg'>Players</h1>
+          <div className='row space-around'>
+            {!playerOne &&
+              <PlayerInput
+                label='Player One'
+                onSubmit={(username) => this.handleSubmit('playerOne', username)}
+              />
+            }
+            {!playerTwo &&
+              <PlayerInput
+                label='Player Two'
+                onSubmit={(username) => this.handleSubmit('playerTwo', username)}
+              />
+            }
+          </div>
+        </div>
       </>
     )
   }
