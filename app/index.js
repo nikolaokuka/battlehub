2f845b477a8bd9425e7c7d6a9ab7bc0c432256be
Nikola Okuka <nikolaokuka@gmail.com>
2021-10-06 20:00:20 +0200

    add ThemeProvider

--- app/index.js
+++ app/index.js
@@ -5,12 +5,25 @@ import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
 
+import { ThemeProvider } from './contexts/theme'
+
 class App extends Component {
+  state = {
+    theme: 'light',
+    toogleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === 'light' ? 'dark' : 'light'
+      }))
+    }
+  }
+
   render() {
     return (
-      <div className='container'>
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state.theme}>
+        <div className='container'>
+          <Battle />
+        </div>
+      </ThemeProvider>
     )
   }
 }
