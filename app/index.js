6f47d0496e49f264537b83a8d41e6c26ddb4816b
Nikola Okuka <nikolaokuka@gmail.com>
2021-10-07 21:46:18 +0200

    code split

--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component, Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './index.css'
 
-import Popular from './components/Popular'
-import Battle from './components/Battle'
 import Nav from './components/Nav'
-import Results from './components/Results'
-
+import Loading from './components/Loading'
 import { ThemeProvider } from './contexts/theme'
+
+const Popular = React.lazy(() => import('./components/Popular'))
+const Battle = React.lazy(() => import('./components/Battle'))
+const Results = React.lazy(() => import('./components/Results'))
 
 class App extends Component {
   state = {
@@ -27,12 +28,15 @@ class App extends Component {
           <div className={this.state.theme}>
             <div className='container'>
               <Nav />
-              <Switch>
-                <Route exact path='/' component={Popular} />
-                <Route exact path='/battle' component={Battle} />
-                <Route path='/battle/results' component={Results} />
-                <Route render={() => <h1 className='center-text'>404</h1>} />
-              </Switch>
+
+              <Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route exact path='/' component={Popular} />
+                  <Route exact path='/battle' component={Battle} />
+                  <Route path='/battle/results' component={Results} />
+                  <Route render={() => <h1 className='center-text'>404</h1>} />
+                </Switch>
+              </Suspense>
             </div>
           </div>
         </ThemeProvider>
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
     filename: 'index_bundle.js',
     publicPath: '/'
   },
-  devtool: 'inline-source-map',
   devServer: {
     port: 3000,
     hot: true,
@@ -23,7 +22,10 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-class-properties']
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-syntax-dynamic-import'
+            ]
           }
         }
       },
